9e3442689838b10c87c61454ddb630c122a22603
Xuan-Anh Tran <xuan-anhtran@Xuan-Anhs-MacBook-Air.local>
2025-02-08 16:29:35 -0800

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+    'mongodb+srv://test:test@mernexercisetracker.eq5zy.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERNExerciseTracker'
